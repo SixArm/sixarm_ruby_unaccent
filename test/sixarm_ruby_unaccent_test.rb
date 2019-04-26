66adeb3d2d94cf7fe138efb47a22219da42579da
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-04-25 20:28:56 -0700

    Update Minitest syntax

--- test/sixarm_ruby_unaccent_test.rb
+++ test/sixarm_ruby_unaccent_test.rb
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 require "minitest/autorun"
 #require "minitest/benchmark" if ENV["BENCH"]
-require "coveralls"
 require "simplecov"
-Coveralls.wear!
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
+])
 SimpleCov.start
 require "sixarm_ruby_unaccent"
